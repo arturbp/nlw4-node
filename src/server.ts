11355559f4af088c39d97a2c3f5079fbd938d07d
artur-pinheiro <artur.pinheiro@pequenasempresas.net>
2021-02-24 15:50:56 -0300

    Segundo dia de NLW#4

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
+import 'reflect-metadata'
 import express from 'express'
+import './database'
+import { router } from './routes'
 
 const app = express()
 
-const PORT = 3333
+app.use(express.json())
+app.use(router)
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' })
-})
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(3333, () => console.log('Server is running'))
