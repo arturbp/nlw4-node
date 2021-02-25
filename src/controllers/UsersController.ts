5ad6dda6699c761ca60ba006d812f2b039410e80
artur-pinheiro <artur.pinheiro@pequenasempresas.net>
2021-02-24 21:51:19 -0300

    Terceiro dia de NLW#4

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express'
-import { getRepository } from 'typeorm'
-import { User } from '../modules/User'
+import { getCustomRepository } from 'typeorm'
+import { UsersRepository } from '../repositories/UsersRepository'
 
-class UserController {
+class UsersController {
   async create(req: Request, res: Response) {
     const { name, email } = req.body
 
-    const usersRepository = getRepository(User)
+    const usersRepository = getCustomRepository(UsersRepository)
 
     const userAlreadyExists = await usersRepository.findOne({
       email
@@ -25,8 +25,8 @@ class UserController {
 
     await usersRepository.save(user)
 
-    return res.json(user)
+    return res.status(201).json(user)
   }
 }
 
-export { UserController }
+export { UsersController }
